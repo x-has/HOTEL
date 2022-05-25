d634e999847c47011dec1711747a19b09d6e2e3c
__xhas <64027680+eb-ook@users.noreply.github.com>
2022-05-25 16:59:22 +0100

    Add files via upload
    Hotel Search

---
+++ final/page official/go/card.js
@@ -0,0 +1,34 @@
+var date = new Date();
+
+var day = date.getDate();
+var month = date.getMonth() + 1;
+var year = date.getFullYear();
+
+if (month < 10) month = "0" + month;
+if (day < 10) day = "0" + day;
+
+var today = year + "-" + month + "-" + day;
+
+
+document.getElementById('date').value = today;
+
+//  NUMBER STEPPER
+
+Array.prototype.slice.call(document.querySelectorAll('.quantity-container'))
+	.map(function (container) {
+    return {
+      input: container.querySelector('.quantity-amount'),
+      decrease: container.querySelector('.decrease'),
+      increase: container.querySelector('.increase'),
+      get value () { return parseInt(this.input.value); },
+      set value (v) { this.input.value = v; }
+    }
+  })
+  .forEach(function (item) {
+    item.decrease.addEventListener('click', function () { 
+      item.value -= 1;
+    });
+    item.increase.addEventListener('click', function () {
+      item.value += 1;
+    });
+  });
